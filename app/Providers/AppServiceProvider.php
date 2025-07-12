77da67e3d3459ddbf8193c447c11c7ec2035d2fb
Radowan30 <radowanahmed30@gmail.com>
2025-07-13 02:46:42 +0800

    updated AppServiceProvider.php file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use DB;
 use Illuminate\Support\ServiceProvider;
 use Log;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
         // \DB::listen(function ($query) {
         //     \Log::info($query->sql, $query->bindings, $query->time);
         // });
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         DB::listen(function ($query) {
             if (str_contains($query->sql, 'insert into `sensors_data`')) {
                 Log::info('Insert query detected on sensors_data table:', [
