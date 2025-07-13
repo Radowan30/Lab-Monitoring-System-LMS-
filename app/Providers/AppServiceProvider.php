5ca95620f755e2e8f43410595cedbbac1b8cdea7
Radowan30 <radowanahmed30@gmail.com>
2025-07-13 15:23:44 +0800

    added a new line to AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
         // });
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
         }
 
         DB::listen(function ($query) {
